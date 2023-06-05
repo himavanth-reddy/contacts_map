a7f93dfe72aa9d1b9ef0a759d624db73e7a6f889
Himavanth Reddy <himavanth93@gmail.com>
2023-06-05 15:11:07 +0530

    added contacts feature

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+import "./index.css";
+import Layout from "./components/Layout";
+import Contacts from "./components/addressBook/Contacts";
+import ChartMap from "./components/chartsMap/ChartMap";
+import PageNotFound from "./components/PageNotFound";
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    errorElement: <PageNotFound />,
+    children: [
+      {
+        path: "/",
+        element: <Contacts />,
+      },
+      {
+        path: "map",
+        element: <ChartMap />,
+      },
+    ],
+  },
+]);
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
